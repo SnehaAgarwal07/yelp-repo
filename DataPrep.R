80c5652b0311a48a888af13908f6cea0726cea73
Sneha Agarwal <snehaagarwal@Snehas-MacBook-Pro.local>
2019-03-19 14:00:14 -0400

    Data file

--- DataPrep.R
+++ DataPrep.R
@@ -1,9 +1,17 @@
 con <- dbConnect(RMySQL::MySQL(), host="localhost",user="root",password="root",dbname="Yelp")
 
+dbWriteTable(con,"reviews",reviews)
+dbWriteTable(con,"business",business)
+dbWriteTable(con,"business_attr",business_attr,row.names=FALSE,append=TRUE)
+dbWriteTable(con,"users",users)
+
+
 business <- tbl(con,"business")
 reviews <- tbl(con, "reviews")
 business_attr <- tbl(con,"business_attr")
 users <- tbl(con,"users")
+
+
 
 business <- rename(business, stars_b = stars)
 
